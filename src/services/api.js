f0c084e697a5f7419ee50bc9b9c67e542e2db3ae
gndbabu <delhibabu44@gmail.com>
2026-01-15 12:49:20 +0100

    apply env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:8080/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL + "/api" || "http://localhost:8080/api";
 
 const apiFetch = async (endpoint, options = {}) => {
   const url = `${API_BASE_URL}${endpoint}`;
